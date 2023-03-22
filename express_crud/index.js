c1dadb01aa8492d809f12f456a2b0a53e7113e80
Sagar Kurewar <sagar.kurewar@indusnet.co.in>
2023-03-22 15:39:59 +0530

    adding chnages

--- express_crud/index.js
+++ express_crud/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const connect = require("./config/db");
 const app = express();
-
+var fs = require('fs')
+var path = require('path')
+const morgan = require('morgan')
 const userController = require('./src/controllers/user.controller.js')
+const PORT = 4000;
+
+// app.use(morgan('combined')) //HTTP request logger middleware for node.js
+ 
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
 
 app.use(express.json());
 app.use('/user', userController)
-
-const PORT = 4000
 
 app.listen(PORT, async () => {
     await connect()
